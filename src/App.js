fdb4461c77bd447367ad2f9aba1869b8fb372417
MPelyhes <mattpelyhes@gmail.com>
2021-03-08 17:01:53 -0600

    create dropdown component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-// import Accordion from './components/Accordion';
-import Search from './components/Search';
+import React, { useState } from 'react';
+// // import Accordion from './components/Accordion';
+// import Search from './components/Search';
+import Dropdown from './components/Dropdown'
 
-const items = [
+// const items = [
+//   {
+//     title: 'What is React?',
+//     content: 'React is a front end JS framework'
+//   },
+//   {
+//     title: 'Why use React',
+//     content: 'React is a favorite JS library among engineers'
+//   },
+//   {
+//     title: 'How do you use React?',
+//     content: 'You use React by creating components'
+//   }
+// ]
+
+const options = [
   {
-    title: 'What is React?',
-    content: 'React is a front end JS framework'
+    label: 'The Color Red',
+    value: 'red'
   },
   {
-    title: 'Why use React',
-    content: 'React is a favorite JS library among engineers'
+    label: 'The Color Green',
+    value: 'green'
   },
   {
-    title: 'How do you use React?',
-    content: 'You use React by creating components'
+    label: 'A Shade of Blue',
+    value: 'blue'
   }
-]
+];
 
 export default function App() {
+  const [selected, setSelected] = useState(options[0]);
+
   return (
     <div>
-      <Search />
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options} />
     </div>
   )
 }
