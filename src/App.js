298dc8e41916f74a54ed36fdcbba5daaca152c6e
MPelyhes <mattpelyhes@gmail.com>
2021-03-09 17:04:00 -0600

    create routing components for navigating between components

--- src/App.js
+++ src/App.js
@@ -1,44 +1,86 @@
 import React, { useState } from 'react';
-// // import Accordion from './components/Accordion';
-// import Search from './components/Search';
-// import Dropdown from './components/Dropdown'
+import Accordion from './components/Accordion';
+import Search from './components/Search';
+import Dropdown from './components/Dropdown'
 import Translate from './components/Translate';
+import Route from './components/Route';
 
-// const items = [
-//   {
-//     title: 'What is React?',
-//     content: 'React is a front end JS framework'
-//   },
-//   {
-//     title: 'Why use React',
-//     content: 'React is a favorite JS library among engineers'
-//   },
-//   {
-//     title: 'How do you use React?',
-//     content: 'You use React by creating components'
-//   }
-// ]
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a front end JS framework'
+  },
+  {
+    title: 'Why use React',
+    content: 'React is a favorite JS library among engineers'
+  },
+  {
+    title: 'How do you use React?',
+    content: 'You use React by creating components'
+  }
+]
 
-// const options = [
-//   {
-//     label: 'The Color Red',
-//     value: 'red'
-//   },
-//   {
-//     label: 'The Color Green',
-//     value: 'green'
-//   },
-//   {
-//     label: 'A Shade of Blue',
-//     value: 'blue'
-//   }
-// ];
+const options = [
+  {
+    label: 'The Color Red',
+    value: 'red'
+  },
+  {
+    label: 'The Color Green',
+    value: 'green'
+  },
+  {
+    label: 'A Shade of Blue',
+    value: 'blue'
+  }
+];
+
+const showAccordion = () => {
+  if(window.location.pathname === '/') {
+    return <Accordion items={items} />
+  }
+};
+
+const showList = () => {
+  if(window.location.pathname === '/list') {
+    return <Search />
+  }
+}
+
+const showDropdown = () => {
+  if(window.location.pathname === '/dropdown') {
+    return <Dropdown options={options} />
+  }
+}
+
+const showTranslate = () => {
+  if(window.location.pathname === '/translate') {
+    return <Translate />
+  }
+}
 
 export default function App() {
-
+  const [selected, setSelected] = useState(options[0]);
+  
   return (
     <div>
-      <Translate />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a Color"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   )
 }
